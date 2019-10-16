94c6b8c313ef4dbb539e9b45ccc78c8eb35c545c
ssthedarklord <ssthechosenone@gmail.com>
2019-10-17 01:48:10 +0530

    Buying Apples!

---
+++ ABA12C.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define FORD(i,a,b) for(int i=a;i>=b;i--)
+#define REP(i,n) FOR(i,0,n)
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define TR(i,a) for( typeof(a.begin()) i=a.begin();i!=a.end();i++)
+#define pc putchar
+#define gc getchar
+#define INF 0x3f3f3f3f
+#define INFL 0x3f3f3f3f3f3f3f3fLL
+#define MOD 1000000007
+#define LIMIT 100050
+#define MAX 1000009
+using namespace std;
+typedef long long ll; typedef long double ld; typedef unsigned long long ull;
+typedef vector<int> vi; typedef vector<ll> vl;
+typedef pair<int, int> pii; typedef pair<ll, ll> pll;
+typedef vector<pair<int, int> > vpii; typedef vector<pair<ll, ll> > vpll;
+typedef vector< vector< pair<int, int> > > vvpii; typedef vector< vector< pair<ll, ll> > > vvpll;
+typedef vector< vector < int > > vvi; typedef vector< vector < ll > > vvl;
+typedef vector< bool > vb;
+inline ll read()
+{
+	ll x=0,neg=0;
+	register ll c=gc();
+	for(;((c<48 || c>57) && c!='-');c=gc());
+	if(c=='-') { neg=1; c=gc();}
+	for(;c>=48 && c<=57;c=gc()) x=(x<<1)+(x<<3)+c-48;
+	if(neg==1) return -x;
+	return x;
+}
+inline void write(ll n, char ch='\n')
+{
+    ll s=n,rev,c=0;
+    if(s<0) { pc('-'); s=-s; }
+    rev=s;
+    if(!s){ pc('0'); pc(ch); return;}
+    while(!(rev%10)){ c++; rev/=10;}
+    rev=0;
+    while(s) { rev=(rev<<3)+(rev<<1)+s%10; s/=10; }
+    while(rev){ pc(rev%10+'0'); rev/=10;}
+    while(c--) pc('0');
+    pc(ch);
+}
+int main()
+{
+    int t=read();
+    while(t--)
+    {
+        int n=read(), k=read(), price[k+1], val[k+1];
+        price[0]=0;
+        FOR(i, 1, k+1)
+            price[i]=read();
+        val[0]=0;
+        FOR(i, 1, k+1)
+        {
+            val[i]=INF;
+            FOR(j, 1, i+1)
+                if(price[j]!=-1)
+                {
+                    int a=0, min_val=INF;
+                    while(a*j<=i)
+                    {
+                        min_val=min(min_val, a*price[j]+val[i-a*j]);
+                        a++;
+                    }
+                    val[i]=min(val[i], min_val);
+                }
+        }
+        val[k]!=INF?write(val[k]):write(-1);
+    }
+    return 0;
+}
+
