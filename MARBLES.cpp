1992ba857377362fa17d942e6ec3e2bddc663092
Soham Satyadharma <ssthechosenone@gmail.com>
2020-04-25 15:15:08 +0530

    Marbles

---
+++ MARBLES.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define FORD(i,a,b) for(int i=a;i>=b;i--)
+#define REP(i,n) FOR(i,0,n)
+#define PB push_back
+#define TR(i,a) for( typeof(a.begin()) i=a.begin();i!=a.end();i++)
+#define pc putchar_unlocked
+#define gc getchar_unlocked
+using namespace std;
+typedef long long int lld;
+inline lld read()
+{
+	lld x=0,neg=0;
+	register lld c=gc();
+	for(;((c<48 || c>57) && c!='-');c=gc());
+	if(c=='-') { neg=1; c=gc();}
+	for(;c>=48 && c<=57;c=gc()) x=(x<<1)+(x<<3)+c-48;
+	if(neg==1) return -x;
+	return x;
+}
+inline void write(lld n)
+{
+    lld s=n,rev,c=0;
+    if(s<0) { pc('-'); s=-s; }
+    rev=s;
+    if(!s){ pc('0'); pc('\n'); return;}
+    while(!(rev%10)){ c++; rev/=10;}
+    rev=0;
+    while(s) { rev=(rev<<3)+(rev<<1)+s%10; s/=10; }
+    while(rev){ pc(rev%10+'0'); rev/=10;}
+    while(c--) pc('0');
+    pc('\n');
+}
+lld C(int n,int r)
+{
+    if(r>n-r) r=n-r;
+    lld ans=1;
+    for(int i=0;i<r;i++) ans=ans*(n-i)/(i+1);
+    return ans;
+}
+int main()
+{
+    int t,n,k;
+    t=read();
+    while(t--)
+    {
+        n=read();
+        k=read();
+        write(C(n-1,k-1));
+    }
+    return 0;
+}
